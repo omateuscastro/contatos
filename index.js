d9c6cf1f2d4b70391900150a09b6b0a7baefa320
unknown <mcastro.mateus@gmail.com>
2020-09-20 21:17:41 -0300

    Crud de Categorias.

--- index.js
+++ index.js
@@ -7,30 +7,77 @@ const Contatos = require('./bd/Contatos');
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 conexao.authenticate();
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.get("/categorias", function(req, res) {
-  res.render("categorias/categorias");
+app.get("/categorias/lista/:mensagem?", function (req, res) {
+  Categorias
+      .findAll({ order: ["descricao"] }) 
+      .then(function (categorias) {
+          if(req.params.mensagem)
+              res.render("categorias/categorias", { categorias: categorias,
+                  mensagem: "Não foi possível, pois já há um contato relacionado a esta categoria."});
+          else
+              res.render("categorias/categorias", { categorias: categorias, mensagem: ""});
+      });
 });
 
-app.get("/categorias/novo", function(req, res) {
-  let mensagens = [];
-
-  res.render("categorias/novo", { mensagens });
+app.get("/categorias/novo", function (req, res) {
+  res.render("categorias/novo", { mensagem: '' });
 });
 
-app.get("/contatos", function(req, res) {
+app.post("/categorias/salvar", function (req, res) {
+  let descricao = req.body.descricao;
+  Categorias.create({
+    descricao
+  }).then(res.render("categorias/novo", { mensagem: "Categoria Incluída." }))
+})
+
+app.get("/categorias/editar/:id", function (req, res) {
+  let id = req.params.id;
+  Categorias
+      .findByPk(id)
+      .then(function (categoria) {
+          res.render("categorias/editar", { categoria: categoria });
+      });
+});
+
+app.post("/categorias/atualizar", function (req, res) {
+  let id = req.body.id;
+  let descricao = req.body.descricao;
+  Categorias
+      .update({ descricao: descricao }, { where: { id: id } })
+      .then(function () {
+          res.redirect("/categorias/lista");
+      });
+});
+
+app.get("/categorias/excluir/:id", function (req, res) {
+  let id = req.params.id;
+  Categorias
+      .destroy({ where: { id: id } })
+      .then(function () {
+          res.redirect("/categorias/lista");
+      })
+      .catch(function(erro){
+          if(erro instanceof Sequelize.ForeignKeyConstraintError) {
+              res.redirect("/categorias/lista/erro");
+          }
+      });
+});
+
+
+app.get("/contatos", function (req, res) {
   res.render("contatos/contatos");
 });
 
-app.get("/contatos/novo", function(req, res) {
+app.get("/contatos/novo", function (req, res) {
   let mensagens = [];
 
   res.render("contatos/novo", { mensagens });
