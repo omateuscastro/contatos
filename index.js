689874552f912b35dbe6aba48ae6de5ce79d15d9
unknown <mcastro.mateus@gmail.com>
2020-09-20 21:36:17 -0300

    Crud de Contatos.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const conexao = require('./bd/conexao');
 const Categorias = require('./bd/Categorias');
 const Contatos = require('./bd/Contatos');
+const formataData = require('./public/js/util');
 
 const app = express();
 
@@ -73,14 +74,69 @@ app.get("/categorias/excluir/:id", function (req, res) {
 });
 
 
-app.get("/contatos", function (req, res) {
-  res.render("contatos/contatos");
+app.get("/contatos/lista", function (req, res) {
+  Contatos
+      .findAll({ order: ["nome"], include: [{ model: Categorias }] })
+      .then(function (contatos) {
+          res.render("contatos/contatos", { contatos: contatos, formataData: formataData });
+      });
 });
 
-app.get("/contatos/novo", function (req, res) {
-  let mensagens = [];
+app.get("/contatos/novo/:mensagem?", function (req, res) {
+  Categorias
+      .findAll({ order: ["descricao"] })
+      .then(function (categorias) {
+          if (req.params.mensagem)
+              res.render("contatos/novo", { mensagem: "Contato incluído.", categorias: categorias });
+          else
+              res.render("contatos/novo", { mensagem: "", categorias: categorias });
+      });
+});
 
-  res.render("contatos/novo", { mensagens });
+app.post("/contatos/salvar", function (req, res) {
+  let nome = req.body.nome;
+  let email = req.body.email;
+  let nascimento = req.body.nascimento;
+  let categoria = req.body.categoria;
+  Contatos
+      .create({ nome: nome, email: email, nascimento: nascimento, categoriaId: categoria })
+      .then(
+          res.redirect("/contatos/novo/incluido")
+      );
+});
+
+app.get("/contatos/editar/:id", function (req, res) {
+  let id = req.params.id;
+  Contatos
+      .findByPk(id)
+      .then(function (contato) {
+          Categorias.findAll()
+              .then(function (categorias) {
+                  res.render("contatos/editar", { contato: contato, categorias: categorias, formataData: formataData });
+              })
+      });
+});
+
+app.post("/contatos/atualizar", function (req, res) {
+  let id = req.body.id;
+  let nome = req.body.nome;
+  let email = req.body.email;
+  let nascimento = req.body.nascimento;
+  let categoria = req.body.categoria;
+  Contatos
+      .update({ nome: nome, email: email, nascimento: nascimento, categoriaId: categoria }, { where: { id: id } })
+      .then(function () {
+          res.redirect("/contatos/lista");
+      });
+});
+
+app.get("/contatos/excluir/:id", function (req, res) {
+  let id = req.params.id;
+  Contatos
+      .destroy({ where: { id: id } })
+      .then(function () {
+          res.redirect("/contatos/lista");
+      })
 });
 
 app.listen(3000);
